8943b83c5836f76705338e9d0bfce219631e494e
Mohammad Affan Shofi <affan8971@gmail.com>
2025-05-29 01:33:59 +0700

    Fix: HTTPS form action and trust proxy headers for Render deployment

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,11 +7,9 @@ use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
+    // Render menggunakan proxy, jadi kita harus mempercayai semuanya
     protected $proxies = '*';
 
-    protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO;
+    // Gunakan hanya HEADER_X_FORWARDED_PROTO untuk deteksi HTTPS
+    protected $headers = Request::HEADER_X_FORWARDED_PROTO;
 }
