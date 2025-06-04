29e723a70c265e0f8721d26bd4ffbf5091495d31
Mohammad Affan Shofi <affan8971@gmail.com>
2025-06-05 04:54:39 +0700

    try to delete .gitkeep, delete something in web.php add something in .gitignore

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use App\Http\Controllers\PrayTimeController;
 
 Route::get('/', [PrayTimeController::class, 'index']);
 Route::post('/praytime', [PrayTimeController::class, 'getPrayTime'])->name('praytime.fetch');
-
-Route::get('/debug', function () {
-    return response()->json([
-        'env' => app()->environment(),
-        'debug' => config('app.debug'),
-        'key' => config('app.key'),
-    ]);
-});
