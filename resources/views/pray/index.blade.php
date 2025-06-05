bd3cfce6e2527875eb28d31936680b27712176a1
Mohammad Affan Shofi <affan8971@gmail.com>
2025-06-05 12:33:05 +0700

    test autodeploy

--- resources/views/pray/index.blade.php
+++ resources/views/pray/index.blade.php
@@ -192,7 +192,7 @@
         </div>
 
         <!-- Judul -->
-        <h1 class="display-5 fw-bold">🕌 PrayTimeNow</h1>
+        <h1 class="display-5 fw-bold">🕌 PrayTime</h1>
         <p class="lead">Cek Jadwal Sholat Akurat Berdasarkan Lokasi dan Tanggal</p>
     </div>
 
